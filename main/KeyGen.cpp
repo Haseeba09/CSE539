8d920900e5a5954935982f917032bcb8f4b1bcd4
Haseeba09 <haseeba09@gmail.com>
2018-11-27 05:22:44 -0700

    key stored to file

---
+++ main/KeyGen.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+string keyGen(int p, int q, unsigned long long  x){
+    string key = "";
+    int M = p*q;
+    int binaryKey[128];
+    int brokenUpBinaryKey[16][8];
+    unsigned char partialKey[16];
+
+    //Get random bits using BBS
+    for(int i=0; i<128; i++){
+        unsigned long long  temp = x;
+        int output = 0;
+        temp = temp*temp;
+        x = temp % M;
+        output = x%2;
+        binaryKey[i] = output;
+    }
+    //Break up binary key into 16 peices of 8 bits
+    int n =0;
+    for(int i=0; i<128; i++){        
+        if(i>1 && i%8 == 0){
+            n++;
+        }
+        brokenUpBinaryKey[n][i%8] = binaryKey[i];       
+    }
+    //and each peice of the key to a byte array, converting from
+    //binary to dec
+    for(int i=0; i<16; i++){
+        partialKey[i] = 0;
+        for(int n=0; n<8; n++){
+            int exponent=1;
+            for(int m=0; m<n; m++){
+                exponent *=2;
+            }
+            partialKey[i] += brokenUpBinaryKey[i][n]*exponent;
+        }
+    }
+    //concatenate each byte into a string
+    for(int i=0; i<16; i++){
+        key+=partialKey[i];
+    }
+
+    return key;
+}
