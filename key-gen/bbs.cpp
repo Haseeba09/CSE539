2dd21dcffddff8cb5c075a4b92dc4e6418d3e61c
Haseeba09 <haseeba09@gmail.com>
2018-11-27 03:33:42 -0700

    full key return as string

--- key-gen/bbs.cpp
+++ key-gen/bbs.cpp
@@ -1,45 +1,88 @@
 #include <iostream>
 using namespace std;
 
-unsigned char partialKeyGen(int p, int q,unsigned long long x);
+string partialKeyGen(int p, int q,unsigned long long x);
 
 int main(){
     int p = 5651;
     int q = 5623;
     unsigned long long  x = 123;
 
-    unsigned char key[30];
+    string key;
     
-    key[0] = partialKeyGen(p,q,x);
-
-    for(int i=1; i<16; i++){
-        key[i] = partialKeyGen(p,q,key[i-1]);
-    }
-
-    for(int i=0; i<16; i++){
-        cout<<(int)key[i]<<endl;
-    }
+    key = partialKeyGen(p,q,x);
+    cout<<key<<endl;
 
     return 0;
 }
 
-unsigned char partialKeyGen(int p, int q, unsigned long long  x){
-    unsigned char partialKey = 0x00;
+string partialKeyGen(int p, int q, unsigned long long  x){
+    string key = "";
     int M = p*q;
+    int binaryKey[128];
+    int brokenUpBinaryKey[16][8];
+    unsigned char partialKey[16];
 
-    for(int i=0; i<8; i++){
+    for(int i=0; i<128; i++){
         unsigned long long  temp = x;
         int output = 0;
         temp = temp*temp;
-        cout<<"temp: "<<temp<<endl;
+        //cout<<"temp: "<<temp<<endl;
         x = temp % M;
         output = x%2;
-        int exponent=1;
-        for(int n=0; n<i; n++){
-            exponent *=2;
+        binaryKey[i] = output;
+        // int exponent=1;
+        // for(int n=0; n<i; n++){
+        //     exponent *=2;
+        // }
+        // partialKey += output*exponent;
+    }
+    
+    for(int i=0; i<128; i++){
+        cout<<binaryKey[i];
+    }
+    int n =0;
+    for(int i=0; i<128; i++){        
+        if(i>1 && i%8 == 0){
+            n++;
         }
-        partialKey += output*exponent;
+        brokenUpBinaryKey[n][i%8] = binaryKey[i];
+        // cout<<"\n binKey = "<<binaryKey[i];
+        // cout<<"\n broken"<<n<<" "<<i%8<<" = "<<brokenUpBinaryKey[n][i%8];        
+    }
+    cout<<endl;
+    cout<<"next"<<endl;
+
+    for(int i =0; i<16; i++){
+        for(int n=0; n<8; n++){
+            cout<<brokenUpBinaryKey[i][n];
+        }
+        
+    }
+    cout<<endl;
+
+    for(int i=0; i<16; i++){
+        // int exponent=1;
+        // for(int n=0; n<i; n++){
+        //     exponent *=2;
+        // }
+        partialKey[i] = 0;
+        for(int n=0; n<8; n++){
+            int exponent=1;
+            for(int m=0; m<n; m++){
+                exponent *=2;
+            }
+            partialKey[i] += brokenUpBinaryKey[i][n]*exponent;
+        }
+    cout<<partialKey[i]<<endl;
     }
 
-    return partialKey;
+    for(int i=0; i<16; i++){
+        key+=partialKey[i];
+    }
+    
+
+
+
+    return key;
 }
